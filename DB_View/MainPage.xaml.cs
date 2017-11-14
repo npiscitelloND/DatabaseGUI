437c73a1e6a8925a9ed8944a21dedc079726b391
npiscitello <nb.piscitello@gmail.com>
2017-11-13 21:09:57 -0500

    Got the pictures laid out nicely - I think? Still on the fence about the labels though...

--- DB_View/MainPage.xaml.cs
+++ DB_View/MainPage.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -26,8 +27,11 @@ namespace DB_View
         {
             this.InitializeComponent();
             name.Text = "Component Name";
-            price.Text = "$Price";
             description.Text = "This is a description. It should stay pretty concise; extended info belongs in the notes section.";
+
+            BitmapImage placeholder = new BitmapImage(new Uri(this.BaseUri, "/Assets/Square150x150Logo.png"));
+            symbol.Source = placeholder;
+            footprint.Source = placeholder;
         }
     }
 }
